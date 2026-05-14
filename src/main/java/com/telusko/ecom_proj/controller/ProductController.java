58eedf597f452f6cc63410f82089e3300af5783e
lensen-degife <lensendegife@gmail.com>
2026-05-14 00:00:20 -0700

    some update

--- src/main/java/com/telusko/ecom_proj/controller/ProductController.java
+++ src/main/java/com/telusko/ecom_proj/controller/ProductController.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5173"})
 @RequestMapping("/api")
 public class ProductController {
 
